6234ffa5774f72dd81be7447c8cf57ec758e8c0b
Rafael Iwao <rafael.iwao@gmail.com>
2019-12-10 17:49:05 -0300

    Module exercises 1

---
+++ JSRocketseat/module1/index.js
@@ -0,0 +1,105 @@
+//-------------------// EXERCICE 1 //-------------------//
+
+var address = {
+    street: "Rua dos pinheiros",
+    number: 1293,
+    district: "Centro",
+    city: "São Paulo",
+    state: "SP"
+   };
+
+   
+function returnAddress(obj){
+
+    return `O usuário mora em ${obj.city} / ${obj.state}, no bairro ${obj.district}, no endereço "${obj.street}" com
+    nº ${obj.number}`;
+}
+
+
+
+
+
+//-------------------// EXERCICE 2 //-------------------//
+
+
+function evenNumbers(firstNumber, lastNumber){
+    while(firstNumber <= lastNumber){
+        if(firstNumber % 2 == 0){
+            console.log(firstNumber)
+        }
+        firstNumber ++;
+    }
+    
+}
+
+
+
+
+//-------------------// EXERCICE 3 //-------------------//
+
+
+function haveSkill(skills) {
+   return (skills.indexOf('Javascript') != -1) ? true : false;
+}
+
+
+var skills = ["Javascript", "ReactJS", "React Native"];
+
+
+
+
+
+
+//-------------------// EXERCICE 4 //-------------------//
+
+function experience(year) {
+    if(year < 1){
+        return 'Iniciante';
+    }
+    else if(year > 1 && year < 3){
+        return 'Intermediário';
+    }
+    else if(year > 3  && year < 7){
+        return 'Avançado';
+    }else{
+        return 'Jedi Master';
+    }
+
+
+}
+var yearsStudy = 7;
+
+
+
+
+//-------------------// EXERCICE 4 //-------------------//
+
+
+
+var users = [
+    {
+        name: "Diego",
+        skills: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        name: "Gabriel",
+        skills: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+];
+
+function returnUser(users){
+    for(user of users){
+        console.log(`O ${user.name} possui as Habilidades: ${user.skills.join(', ')}.cls`)
+    }
+}
+   
+   
+
+
+
+
+// console.log(returnAddress(address))
+// console.log(evenNumbers(32, 321))
+// console.log(haveSkill(skills))
+// console.log(experience(yearsStudy));
+// returnUser(users);   
